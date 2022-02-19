76b7ecbc6a647f9efc58e2d948f71f32a63fb7db
Conrad_Murphy <chm73@nau.edu>
2022-02-19 13:40:36 -0700

    Update ChatClient.java
    Tasks needed: Client methods, proper interaction between main loop and ClientSender/Receiver

--- Project 2/src/chat/ChatClient.java
+++ Project 2/src/chat/ChatClient.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 import java.util.Properties;
+import java.util.Scanner;
 
 import utils.PropertyHandler;
 
@@ -42,6 +43,8 @@ public class ChatClient
         // Initialize operation flags
         isConnected = false;
         isShutdown = false;
+        Boolean firstJoinAttempt = true;
+        Boolean startedSendAndReceive = false;
 
         // Declare properties file
         String propertiesFile = null;
@@ -51,6 +54,14 @@ public class ChatClient
 
         // Declare server socket properties
         Properties serverProperties = null;
+
+        // Declare user input objects
+        Scanner userInput;
+        String[] inputArray;
+
+        // Declare sender and receiver
+        ClientSender sender;
+        ClientReceiver receiver;
 
         // Attempt to fetch properties file
         try
@@ -79,11 +90,9 @@ public class ChatClient
         // Attempt to read server socket port
         try
         {
-            serverPort = Integer.parseInt(
-                    serverProperties.getProperty("SERVER_PORT"));
+            serverPort = Integer.parseInt(serverProperties.getProperty("SERVER_PORT"));
         }
-        // Failed to read server port, likely due to port not existing
-        // or not being an integer
+        // Failed to read server port, likely due to port not existing or not being an integer
         catch (NumberFormatException ex)
         {
             // Log failure and exit
@@ -103,6 +112,9 @@ public class ChatClient
             System.exit(1);
         }
 
+        // Create user input scanner
+        userInput = new Scanner(System.in);
+
         //////////////////////////////////
         // BEGIN MAIN SEND/RECEIVE LOOP //
         //////////////////////////////////
@@ -110,16 +122,107 @@ public class ChatClient
         // Run client until shutdown is received
         while (!isShutdown)
         {
-            // If client is not connected, attempt to connect to a chat
-            // until connection succeeds
+            // Client is not connected. Attempt to connect to a chat until connection succeeds
             if (!isConnected)
             {
+                // Print join instructions, but only the first time
+                if (firstJoinAttempt)
+                {
+                    System.out.println("\nCurrently not connected to a chat server.");
+                    System.out.println("Please enter a client to which you would like to connect.");
+
+                    // Set join attempt flag
+                    firstJoinAttempt = false;
+                }
+
+                // These instructions will ALWAYS be printed on every join
+                // attempt.
+                System.out.println("Use the following format:");
+                System.out.println("- 'JOIN' to create a new chat room");
+                System.out.println("- 'JOIN <IP Address> <Port Number>' to join an existing chat room");
+                System.out.println("- 'SHUTDOWN' to exit.\n");
                 
+
+                // Read next command and split it into its arguments
+                inputArray = userInput.nextLine().split(" ");
+
+                // Case JOIN command
+                if (inputArray[0].equals("JOIN"))
+                {
+                    // Check for standalone JOIN command
+                    if (inputArray.length == 1)
+                    {
+                        System.out.println("Created new chat room.\n");
+                    }
+                    // Check for three arguments--JOIN, IP, port
+                    else if (inputArray.length == 3)
+                    {
+                        try
+                        {
+                            // Attempt to join: joinToChat
+                            isConnected = joinToChat(inputArray[1], Integer.parseInt(inputArray[2]));
+                            
+                            // Connection success
+                            if (isConnected)
+                            {
+                                System.out.println("Successfully joined to existing chat room.\n");
+                            }
+                            // Connection failed
+                            else
+                            {
+                                System.out.println("Could not join existing chat room.\n");
+                            }
+                        }
+                        // Failed to read server port
+                        catch (NumberFormatException ex)
+                        {
+                            System.out.println("Failed to read port number.");
+                        }
+                        
+                    }
+                    // Invalid JOIN command
+                    else
+                    {
+                        System.out.println("Invalid number of arguments for JOIN.");
+                    }
+                }
+                // Case SHUTDOWN command
+                else if (inputArray[0].equals("SHUTDOWN"))
+                {
+                    // Print shutdown
+                    System.out.println("Shutting down client.\n");
+
+                    // Set shutdown flag
+                    isShutdown = true;
+                }
+                // Invalid command input
+                else
+                {
+                    System.out.println("Invalid command.");
+                }
+
+            } // End "if not connected" block
+
+            // Client has connected but has not yet created its sender and receiver. This happens
+            // exactly once after a join succeeds.
+            else if (!startedSendAndReceive)
+            {
+                sender = new ClientSender();
+                receiver = new ClientReceiver();
+
+                // Set send/receive flag
+                startedSendAndReceive = true;
+
+                // Set first attempt flag to display full instructions after leaving chat
+                firstJoinAttempt = true;
             }
 
-            // If client is connected, remain on standby with sender and
-            // receiver active. Do nothing here in main.
+            // Client is connected. Remain on standby with sender and receiver active. Do nothing here
+            // in main.
         }
+
+        // Close user input stream
+        userInput.close();
     }
 
     public static void addUser(NodeInfo user)
@@ -127,9 +230,9 @@ public class ChatClient
 
     }
 
-    public static void joinToChat(String ip, int port)
+    private static Boolean joinToChat(String ip, int port)
     {
-
+        return false; // TODO
     }
 
     public static void leaveFromChat()
