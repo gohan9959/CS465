5ac8d28a06ee00d947556f11ec8d8fb5f35c94c4
Harhon <30802005+gohan9959@users.noreply.github.com>
2022-02-27 23:19:07 -0700

    Update ClientReceiver.java

--- Project 2/src/chat/ClientReceiver.java
+++ Project 2/src/chat/ClientReceiver.java
@@ -13,53 +13,53 @@ import message.NodeInfo;
 
 public class ClientReceiver implements Runnable, MessageTypes{
 
-    private ServerSocket serverSocket;
+    private ServerSocket serverSocket; // Server socket to recieve connection.
 
-    private Message message;
+    private Message message; // Message object to store recieved messages.
 
-    private NodeInfo userInfo;
+    private NodeInfo userInfo; // NodeInfo object to store user information.
 
     public ClientReceiver(ServerSocket serverSocket, NodeInfo userInfo){
-        this.serverSocket = serverSocket;
-        this.userInfo = userInfo;
+        this.serverSocket = serverSocket; // Storing the ChatCilent created server socket in the server socket variable.
+        this.userInfo = userInfo; // Soring the user info created in the ChatCilent.
     }
 
     public void run(){
-        Socket socket;
-        ObjectInputStream recieveStream;
-        boolean leave = false;
+        Socket socket; // Socket to store accepted connections.
+        ObjectInputStream recieveStream; // ObjectInputStream object to recieve objects from sender.
+        boolean leave = false; // Flag to check if the user has left.
 
-        while(!leave){
-            try{
-                socket = serverSocket.accept();
-                recieveStream = new ObjectInputStream(socket.getInputStream());
-                message = (Message)recieveStream.readObject();
-                if(message instanceof Message){
-                    if(message.getMessageType() == JOIN){
-                        ChatClient.receiveJoiningUser((NodeInfo)message.getMessageContent());
+        while(!leave){ // While the user is still in chat continue to loop. 
+            try{ 
+                socket = serverSocket.accept(); // Trying to accept connections.
+                recieveStream = new ObjectInputStream(socket.getInputStream()); // Tring to recieve object from accepted socket.
+                message = (Message)recieveStream.readObject(); // Trying to read the object recieved.
+                if(message instanceof Message){ // Checking if the recieved object is of type Message or not.
+                    if(message.getMessageType() == JOIN){ // Checking if the message type is JOIN.
+                        ChatClient.receiveJoiningUser((NodeInfo)message.getMessageContent()); // If it is we are calling the recieveJoiningUser method from ChatClient.
                     }
-                    else if(message.getMessageType() == ADD){
-                        ChatClient.addUser((NodeInfo)message.getMessageContent());
+                    else if(message.getMessageType() == ADD){ // Checking if the message type is ADD.
+                        ChatClient.addUser((NodeInfo)message.getMessageContent()); // If it is we are calling the addUser method from ChatClient.
                     }
-                    else if(message.getMessageType() == ADD_LIST){
-                        ChatClient.addList((ArrayList<NodeInfo>) message.getMessageContent());
+                    else if(message.getMessageType() == ADD_LIST){ // Checking if the message type is ADD_LIST.
+                        ChatClient.addList((ArrayList<NodeInfo>) message.getMessageContent()); // If it is we are calling the addList method from ChatClient.
                     }
-                    else if(message.getMessageType() == NOTE){
-                        System.out.println(message.getSender() + ": " + (String)message.getMessageContent());
+                    else if(message.getMessageType() == NOTE){ // Checking if the message type is NOTE.
+                        System.out.println(message.getSender() + ": " + (String)message.getMessageContent()); // If it is we are printing the message with name of the user who sent it.
                     }
-                    else if(message.getMessageType() == LEAVE){
-                        if(message.getSender().equals(userInfo.getLogicalName())){
-                            leave = true;
+                    else if(message.getMessageType() == LEAVE){ // Checking if the message type is LEAVE.
+                        if(message.getSender().equals(userInfo.getLogicalName())){ // Cheking if the leave message is ours.
+                            leave = true; // If it is then we are setting the leave flag to true so that the thread ends.
                         }
-                        ChatClient.removeUser((NodeInfo)message.getMessageContent());
+                        ChatClient.removeUser((NodeInfo)message.getMessageContent()); // Calling removeUser method from ChatClient.
                     }
-                    else if(message.getMessageType() == SHUTDOWN){
-                        ChatClient.receiveShutdown();
-                        leave = true;
+                    else if(message.getMessageType() == SHUTDOWN){ // Checking if the message type is SHUTDOWN.
+                        ChatClient.receiveShutdown(); // If it is we are calling the recieveShutdown method from ChatClient.
+                        leave = true; // Setting the leave flag to true so that the thread ends.
                     }
                 }
             } 
-            catch (IOException e){
+            catch (IOException e){ 
                 e.printStackTrace();
             }
             catch(ClassNotFoundException cfe){
