8af7166cd3369949660d59989f7997894575bfc9
Conrad_Murphy <chm73@nau.edu>
2022-04-03 11:33:50 -0700

    Update account and account manager
    Also update gitignore

--- Project 3/src/account/AccountManager.java
+++ Project 3/src/account/AccountManager.java
@@ -1,6 +1,35 @@
 package account;
 
+import java.util.ArrayList;
+
 public class AccountManager
 {
-    
+    private ArrayList<Account> accountList;
+
+    public AccountManager()
+    {
+        accountList = new ArrayList<Account>();
+    }
+
+    /**
+     * Read balance of account.
+     * 
+     * @param accountID ID of account represented by index in array list.
+     * @return Account's current balance.
+     */
+    public int readAccountBalance(int accountID)
+    {
+        return accountList.get(accountID).getBalance();
+    }
+
+    /**
+     * Write new balance to account.
+     * 
+     * @param accountID ID of account represented by index in array list.
+     * @param newBalance New balance to be written.
+     */
+    public void writeAccountBalance(int accountID, int newBalance)
+    {
+        accountList.get(accountID).setBalance(newBalance);
+    }
 }
